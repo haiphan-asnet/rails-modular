ba04cfe5d95c17ec2c2f0eca17ff1b67240224ed
Hai Phan <hai.phanthanh@asnet.com.vn>
2015-05-21 14:48:23 +0700

    fix for bower_rails config

--- blog/engines/core/lib/blog/core/engine.rb
+++ blog/engines/core/lib/blog/core/engine.rb
@@ -3,19 +3,18 @@ module Blog
     class Engine < ::Rails::Engine
       isolate_namespace Blog
 
-      # Load bower_rails
-      require "#{root}/config/initializers/bower_rails.rb"
+      config.to_prepare do
+        # Bower asset paths
+        Blog::Core::Engine.root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+          Rails.application.config.sass.load_paths << bower_path
+          Rails.application.config.assets.paths << bower_path
+        end
 
-      # Bower asset paths
-      root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
-        config.sass.load_paths << bower_path
-        config.assets.paths << bower_path
+        # Precompile Bootstrap fonts
+        Rails.application.config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+        # Minimum Sass number precision required by bootstrap-sass
+        ::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
       end
-
-      # Precompile Bootstrap fonts
-      config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
-      # Minimum Sass number precision required by bootstrap-sass
-      ::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
     end
   end
 end

98353182d301a140bfadc074289e3d7a7bcba53c
Hai Phan <hai.phanthanh@asnet.com.vn>
2015-05-22 17:11:10 +0700

    use new rspec syntax expose factory_girl methods

--- blog/engines/core/spec/models/blog/user_spec.rb
+++ blog/engines/core/spec/models/blog/user_spec.rb
@@ -4,17 +4,17 @@ module Blog
   RSpec.describe User, type: :model do
     context "factories" do
       it "has a valid factory" do
-        expect(FactoryGirl.build(:blog_user)).to be_valid
+        expect(build(:blog_user)).to be_valid
       end
 
       it "has a valid aliases factory" do
-        expect(FactoryGirl.build(:user)).to be_valid
+        expect(build(:user)).to be_valid
       end
     end
 
     context "validations" do
-      it { should validate_presence_of(:email) }
-      it { should validate_uniqueness_of(:email) }
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_uniqueness_of(:email) }
     end
   end
 end

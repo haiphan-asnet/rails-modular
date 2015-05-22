ac58a14b7be1f1575d0bb71db84a391478d17d33
Hai Phan <hai.phanthanh@asnet.com.vn>
2015-05-22 16:23:43 +0700

    fix User factory email

--- blog/engines/core/spec/factories/blog_users.rb
+++ blog/engines/core/spec/factories/blog_users.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryGirl.define do
   factory :blog_user, class: 'Blog::User', aliases: [:user] do
-    email 'hairbeos@y.com'
+    email { Faker::Internet.email }
     password 123456789
   end
 end
